6d370d08f1663e33eac5567c1a6e8fcd03687321
Aurelie Menato <mnr3492a@u1-105-18d.univ-tlse3.fr>
2023-09-27 10:56:00 +0200

    add high_level models & admin

--- brewsin/high_level/models.py
+++ brewsin/high_level/models.py
@@ -5,9 +5,53 @@ from django.db import models
 class Departement(models.Model):
 	numero = models.IntegerField()
 	prix_m2 = models.IntegerField()
+
+class Ingredient(models.Model):
+	nom = models.CharField(max_length=100)
+
+class QuantiteIngredient(models.Model):
+	ingredient= models.ForeignKey(
+					Ingredient, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)	
+	quantite = models.IntegerField()
 	
+class Machine(models.Model):
+	nom = models.CharField(max_length=100)
+	prix = models.IntegerField()
+	
+class Action(models.Model):
+	machine = models.ForeignKey(
+					Machine, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)
+	commande = models.CharField(max_length=100)
+	duree = models.IntegerField()
+	ingredients = models.ManyToManyField(QuantiteIngredient)
+	action1 = models.ForeignKey(
+					"self",
+					on_delete=models.PROTECT,
+					blank=True, null=True,
+					# related_name="+",
+				)
+	
+	
+class Recette(models.Model):
+	nom = models.CharField(max_length=100)
+	action1 = models.ForeignKey(
+					Action, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)
 
 
+
+	
 class Usine(models.Model):
 	departement= models.ForeignKey(
 					Departement, # ou "self"
@@ -36,46 +80,10 @@ class Prix(models.Model):
 				)
 	prix = models.IntegerField()
 
-class Ingredient(models.Model):
-	nom = models.CharField(max_length=100)
-
-class QuantiteIngredient(models.Model):
-	ingredient= models.ForeignKey(
-					Ingredient, # ou "self"
-					on_delete=models.PROTECT,
-					# blank=True, null=True,
-					# related_name="+",
-				)	
-	quantite = models.IntegerField()
 
 
-class Action(models.Model):
-	machine = models.ForeignKey(
-					Machine, # ou "self"
-					on_delete=models.PROTECT,
-					# blank=True, null=True,
-					# related_name="+",
-				)
-	commande = models.CharField(max_length=100)
-	duree = models.IntegerField()
-	ingredients = models.ManyToManyField(QuantiteIngredient)
-	action = models.ForeignKey(
-					"self",
-					on_delete=models.PROTECT,
-					blank=True, null=True,
-					# related_name="+",
-				)
-	
-class Recette(models.Model):
-	nom = models.CharField(max_length=100)
-	action = models.ForeignKey(
-					Action, # ou "self"
-					on_delete=models.PROTECT,
-					# blank=True, null=True,
-					# related_name="+",
-				)
-class Machine(models.Model):
-	nom = models.CharField(max_length=100)
-	prix = models.IntegerField()
+
+
+
 	
 	
