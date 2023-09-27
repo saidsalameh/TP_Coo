27ba1e4c16b9593e5c969a80d2b2720522aa4bab
Aurelie Menato <mnr3492a@u1-105-18d.univ-tlse3.fr>
2023-09-27 10:43:13 +0200

    add high_level models & admin

--- brewsin/high_level/admin.py
+++ brewsin/high_level/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+admin.site.register(models.Departement)
+admin.site.register(models.Usine)
+admin.site.register(models.Prix)
+admin.site.register(models.Ingredient)
+admin.site.register(models.QuantiteIngredient)
+admin.site.register(models.Action)
+admin.site.register(models.Recette)
+admin.site.register(models.Machine)
+
+
