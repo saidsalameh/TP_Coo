4ac2bbebe3e5ee59da09d1bfed796b81e70da0b5
Aurelie Menato <mnr3492a@u1-105-18d.univ-tlse3.fr>
2023-09-27 10:35:59 +0200

    add high_level models & admin

--- brewsin/high_level/models.py
+++ brewsin/high_level/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
 # Create your models here.
+
+class Departement(models.Model):
+	numero = models.IntegerField()
+	prix_m2 = models.IntegerField()
+	
+
+
+class Usine(models.Model):
+	departement= models.ForeignKey(
+					Departement, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)
+	chriffre = models.IntegerField()
+	machines = models.ManyToManyField(Machine)
+	recettes = models.ManyToManyField(Recette)
+	stocks = models.ManyToManyField(QuantiteIngredient)
+	
+
+class Prix(models.Model):
+	ingredient= models.ForeignKey(
+					Ingredient, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)	
+	departement= models.ForeignKey(
+					Departement, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)
+	prix = models.IntegerField()
+
+class Ingredient(models.Model):
+	nom = models.CharField(max_length=100)
+
+class QuantiteIngredient(models.Model):
+	ingredient= models.ForeignKey(
+					Ingredient, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)	
+	quantite = models.IntegerField()
+
+
+class Action(models.Model):
+	machine = models.ForeignKey(
+					Machine, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)
+	commande = models.CharField(max_length=100)
+	duree = models.IntegerField()
+	ingredients = models.ManyToManyField(QuantiteIngredient)
+	action = models.ForeignKey(
+					"self",
+					on_delete=models.PROTECT,
+					blank=True, null=True,
+					# related_name="+",
+				)
+	
+class Recette(models.Model):
+	nom = models.CharField(max_length=100)
+	action = models.ForeignKey(
+					Action, # ou "self"
+					on_delete=models.PROTECT,
+					# blank=True, null=True,
+					# related_name="+",
+				)
+class Machine(models.Model):
+	nom = models.CharField(max_length=100)
+	prix = models.IntegerField()
+	
+	
